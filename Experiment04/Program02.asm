a897712f508754e49edf5bf632dc85a32eb37c99
Haysten Dcosta <haystendc@gmail.com>
2023-05-09 00:50:27 +0530

    Fixed the incorrect count display bug

--- Experiment04/Program02.asm
+++ Experiment04/Program02.asm
@@ -1,7 +1,7 @@
 ; TO COUNT NO. OF POSITIVE AND NEGATIVE NUMBERS IN ARRAY USING PROCEDURES...
 
 section .data
-    num_array db 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10; Number array containing numbers..
+    num_array db 0, -1, -2, 3, -4, 5, -6, 7, -8, 9, -10 ; Number array containing numbers..
 
     disp_neg_count db 10, "Count of negative numbers : "
     disp_neg_countL equ $ -disp_neg_count
@@ -11,77 +11,76 @@ section .data
     new_lineL equ $ -new_line
 
 section .bss
-    neg_count resb 5
-    pos_count resb 5
+    neg_count resb 1
+    pos_count resb 1
 
 section .text
     global _start
 _start:
 
-    mov bl, 0
-    mov dl, 0
-    mov cl, 11
-    mov esi, num_array
+    MOV BL, 0
+    MOV DL, 0
+    MOV CL, 11
+    MOV ESI, num_array
 
-    CalL CHECK_pos_neg ; Function to count positive and negative count...
+    CALL CHECK_pos_neg
+    MOV EDX, disp_neg_countL
+    MOV ECX, disp_neg_count
+    MOV EBX, 1
+    MOV EAX, 4
+    INT 80H
 
-    mov edx, disp_neg_countL
-    mov ecx, disp_neg_count
-    mov ebx, 1
-    mov eax, 4
-    int 80H
+    MOV EDX, 1
+    MOV ECX, neg_count
+    MOV EBX, 1
+    MOV EAX, 4
+    INT 80H
 
-    mov edx, 5
-    mov ecx, neg_count
-    mov ebx, 1
-    mov eax, 4
-    int 80H
+    MOV EDX, disp_pos_countL
+    MOV ECX, disp_pos_count
+    MOV EBX, 1
+    MOV EAX, 4
+    INT 80H
 
-    mov edx, disp_pos_countL
-    mov ecx, disp_pos_count
-    mov ebx, 1
-    mov eax, 4
-    int 80H
+    MOV EDX, 1
+    MOV ECX, pos_count
+    MOV EBX, 1
+    MOV EAX, 4
+    INT 80H
 
-    mov edx, 5
-    mov ecx, pos_count
-    mov ebx, 1
-    mov eax, 4
-    int 80H
+    MOV EDX, new_lineL
+    MOV ECX, new_line
+    MOV EBX, 1
+    MOV EAX, 4
+    INT 80H
 
-    mov edx, new_lineL
-    mov ecx, new_line
-    mov ebx, 1
-    mov eax, 4
-    int 80H
+    MOV EBX, 0 ; Exit...
+    MOV EAX, 1
+    INT 80H
 
-    mov ebx, 0 ; Exit...
-    mov eax, 1
-    int 80H
-
-CHECK_pos_neg: ; Procedure definition...
+CHECK_pos_neg:
 
 NEXT:
-    mov al, [esi]
-    rol al, 1
-    jc NEGATIVE
-
-    inc bl
-    inc esi
-    dec cl
-    jnz NEXT
-    jmp COUNTER
+    MOV AL, [ESI]
+    ROL AL, 1
+    JC NEGATIVE
+    
+    INC BL
+    INC ESI
+    DEC CL
+    JNZ NEXT
+    JMP COUNTER
 
 NEGATIVE:
-    inc dl
-    inc esi
-    dec cl
-    jnz NEXT
-    jmp COUNTER
-    
+    INC DL
+    INC ESI
+    DEC CL
+    JNZ NEXT
+    JMP COUNTER
+
 COUNTER:
-    add dl, '0'
-    mov [neg_count], dl
-    add bl, '0'
-    mov [pos_count], bl
-    ret
+    ADD DL, '0'
+    MOV [neg_count], DL
+    ADD BL, '0'
+    MOV [pos_count], BL
+    RET
